b5038bdf9cb7417bfcaed74ed59b56f070878ac2
heavenstar0538-sketch <heavenstar0538@gmail.com>
2026-08-19 10:22:30 +0000

    Initialize Telegram bot with basic message handler

---
+++ bot.js
@@ -0,0 +1,9 @@
+hereconst TelegramBot = require('node-telegram-bot-api');
+
+const token = process.env.BOT_TOKEN; // We will set this on Render
+const bot = new TelegramBot(token, {polling: true});
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'Hello! Bot is alive 🔥');
+});
